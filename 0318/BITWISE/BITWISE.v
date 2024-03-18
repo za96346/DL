de94c4ac33be0ab9dafedc1424367cf034cfb561
siou <za96346@gmail.com>
2024-03-18 20:39:07 +0800

    add bitwise

---
+++ 0318/BITWISE/BITWISE.v
@@ -0,0 +1,12 @@
+module BITWISE(A, B, NOT_OP, AND_OP, OR_OP, XOR_OP, NAND_OP, NOR_OP, XNOR_OP);
+    input A, B;
+    output NOT_OP, AND_OP, OR_OP, XOR_OP, NAND_OP, NOR_OP, XNOR_OP;
+
+    assign NOR_OP = ~A;
+    assign AND_OP = A & B;
+    assign OR_OP = A | B;
+    assign XOR_OP = A ^ B;
+    assign NAND_OP = ~(A & B);
+    assign NOR_OP = ~(A | B);
+    assign XNOR_OP = (A ^ ~B);
+endmodule
